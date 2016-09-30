3c6a685305ed4ee746c182d3657b5b1e636d7870
afr1983 <andre.luis.oliveira.freitas@gmail.com>
2016-09-30 19:02:01 -0300

    .

--- worker.js
+++ worker.js
@@ -4,15 +4,12 @@ var kue = require('kue');
 
 var queue = kue.createQueue();
  
-var webhookUri_fail = "https://hooks.slack.com/services/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";
-var webhookUri_success = "https://hooks.slack.com/services/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";
-
-function postSlack(uri, channel, message) {
+function postSlack(channel, message) {
   var slackWebHook = new slack();
-  slackWebHook.setWebhook(uri);
+  slackWebHook.setWebhook("https://hooks.slack.com/services/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");
   slackWebHook.webhook({
-    channel: channel,
-    username: "docker-hub-webhook",
+    channel: "#smartcenter2-lab4",
+    username: "LAB4 Updater",
     text: message
   }, function(err, response) {
     console.log(response);
@@ -32,11 +29,12 @@ queue.process('update-server', function (job, done) {
   try {
     console.log("Worker - starting job - " + job.data.image);
     var result = childProcess.execSync('/srv/app/docker-images-update.sh ' + job.data.image, {encoding: 'ASCII', env: process.env, shell: "/bin/bash"});
-    console.log(result);
-    postSlack(webhookUri_success, "#smartcenter2-errors", "Success updating server");
+    console.log("Success - " + result);
+    postSlack("#smartcenter2-errors", "Success updating server");
     done();
   } catch (error) {
-    postSlack(webhookUri_fail, "#smartcenter2-errors", "Error when updating server - " + error);
+    console.log("Error - " + error);
+    postSlack("#smartcenter2-errors", "Error when updating server - " + error);
     done(error);
   }
 });
